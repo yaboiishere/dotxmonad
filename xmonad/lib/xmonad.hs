57ccbc6927f9af6d3ed69bf2727ff2076ee49e51
Mihail Dobrev <mixaildobrev@gmail.com>
2024-03-25 10:24:05 +0200

    add greenclip

--- xmonad/lib/xmonad.hs
+++ xmonad/lib/xmonad.hs
@@ -134,6 +134,8 @@ myKeys conf@(XConfig {XMonad.modMask = modm}) =
       ((0, xK_Print), spawn "flameshot gui"),
       ((modm, xK_Print), spawn "flameshot screen -p ~/Pictures"),
       ((modm .|. shiftMask, xK_s), spawn "flameshot gui"),
+      -- Clipboard rofi-greenclip
+      ((modm, xK_v), spawn "rofi -modi \"clipboard:greenclip print\" -show clipboard -run-command '{cmd}'"),
       -- Media Keys
       ((0, xF86XK_AudioLowerVolume), spawn "amixer -q set Master 5%-"),
       ((0, xF86XK_AudioRaiseVolume), spawn "amixer -q set Master 5%+"),
@@ -162,7 +164,7 @@ myKeys conf@(XConfig {XMonad.modMask = modm}) =
       -- mod-shift-{w,e,r}, Move client to screen 1, 2, or 3
       --
       [ ((m .|. modm, key), screenWorkspace sc >>= flip whenJust (windows . f))
-        | (key, sc) <- zip [xK_w, xK_e, xK_r] [0 ..],
+        | (key, sc) <- zip [xK_e, xK_w, xK_r] [0 ..],
           (f, m) <- [(W.view, 0), (W.shift, shiftMask)]
       ]
 
